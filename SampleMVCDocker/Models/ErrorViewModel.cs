3047eb51fdfdb58b58a8ecd4f9a62ffcd9ce8788
Raginee Gupta <ragineegupta123@gmail.com>
2026-04-22 23:52:36 +0530

    midd...

---
+++ SampleMVCDocker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SampleMVCDocker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
